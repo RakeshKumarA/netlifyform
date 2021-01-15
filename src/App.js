a8422cd4c3af8fa27819f2e348c0e350dc3edd8a
RakeshKumarA <rkakumar85@gmail.com>
2021-01-15 10:32:18 +0100

    asdf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div>
       <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
-        <TextField label="Name" type="text" name="name" />
-        <Button variant="contained" color="default" type="submit">
-          Submit
-        </Button>
+        <input type="text" name="name" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
