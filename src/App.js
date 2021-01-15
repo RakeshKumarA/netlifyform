838353dac16e1baf8999e9503b64460d70d08ada
RakeshKumarA <rkakumar85@gmail.com>
2021-01-15 12:31:12 +0100

    afda

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <form name="contact" method="post" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="name" />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form name="contact" netlify>
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
 }
 
