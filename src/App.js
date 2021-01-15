b933962ecc78965cd5d37315537ff7587ba6d624
RakeshKumarA <rkakumar85@gmail.com>
2021-01-15 09:57:05 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form name="contact" method="POST" data-netlify="true">
+        <TextField label="Name" type="text" />
+        <Button variant="contained" color="default">
+          Submit
+        </Button>
+      </form>
     </div>
   );
 }
