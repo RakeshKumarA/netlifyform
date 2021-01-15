b72d190d06a528980d1fc5593b9478195028dfd9
RakeshKumarA <rkakumar85@gmail.com>
2021-01-15 12:49:30 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import { Grid, Card, TextField, Button } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    if (window.location.search.includes("success=true")) {
+      setSuccess(true);
+    }
+  }, []);
   return (
-    <div>
-      <form name="contact" method="post" data-netlify="true" onSubmit="submit">
-        <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="name" />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Grid maxWidth="sm">
+      <h2>CONTACT</h2>
+      {success && <p style={{ color: "green" }}>Thanks for your message! </p>}
+      <Card>
+        <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
+          <TextField id="standard-basic" label="name" name="name" />
+          <TextField id="standard-basic" label="email" name="email" />
+          <TextField
+            multiline
+            id="standard-basic"
+            label="message"
+            name="message"
+          />
+          <Button type="submit">Send</Button>
+        </form>
+      </Card>
+    </Grid>
   );
 }
 
