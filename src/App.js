72b157007d43b0a36106b6b0664a5a4263c14d3f
RakeshKumarA <rkakumar85@gmail.com>
2021-01-15 12:29:10 +0100

    adf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+      <form name="contact" method="post" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" />
         <button type="submit">Submit</button>
       </form>
